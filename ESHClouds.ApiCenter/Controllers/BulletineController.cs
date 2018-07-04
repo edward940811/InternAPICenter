e62c62d4f963767af1c06e41add4d6d6484c1515
Kyle Shen <kyle.shen616@gmail.com>
2018-07-04 09:39:06 +0800

    rename bulletine api route name

---
+++ ESHClouds.ApiCenter/Controllers/BulletineController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ESHClouds.ApiCenter.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESHClouds.ApiCenter.Controllers
+{
+    [Route("api/[controller]")]
+    public class BulletineController : BaseController
+    {
+        public BulletineController(ClaimsIdentity identity) : base(identity)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<List<Bulletine>> Get()
+        {
+            return new List<Bulletine>();
+        }
+
+        [HttpGet("{id}", Name = "GetBulletine")]
+        public Bulletine Get(int id)
+        {
+            return new Bulletine();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Bulletine item)
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(Bulletine item)
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
