4e7f913682db077c44dbff2d98f135831db10adf
Edward Yang <eyang12@sundevils.asu.edu>
2018-07-05 12:31:44 +0800

    CRUD test Complete

--- UnitTestProject/ESHCloudsBulletineTest.cs
+++ UnitTestProject/ESHCloudsBulletineTest.cs
@@ -15,7 +15,7 @@ namespace UnitTestProject
         BulletineClass _service = new BulletineClass();
 
         [TestMethod]
-        public void ESHCloudsBulletine正確回傳()
+        public void Bulletine正確建立 ()
         {
             BulletineViewModel model = new BulletineViewModel
             {
@@ -27,6 +27,35 @@ namespace UnitTestProject
                 Module = "Chem",
             };
             string result = _service.Create(model);
+            Assert.AreNotEqual(result, null);
+        }
+        [TestMethod]
+        public void Bulletine正確取得()
+        {          
+            IEnumerable<BulletineViewModel> result = _service.GetAll();
+            Assert.AreNotEqual(result, null);
+        }
+        [TestMethod]
+        public void Bulletine正確更新()
+        {
+            BulletineViewModel model = new BulletineViewModel
+            {
+                Id = 2002,
+                CompanyId = "hahahahahah",
+                Description = "busted",
+                Top = true,
+                Type = "type2",
+                Date = DateTime.Now,
+                Module = "Chem",
+            };
+            string result = _service.Update(model);
+            Assert.AreNotEqual(result, null);
+        }
+        [TestMethod]
+        public void Bulletine正確軟刪除()
+        {
+            int id = 2002;
+            string result = _service.Delete(id);
             Assert.AreNotEqual(result, null);
         }
     }
